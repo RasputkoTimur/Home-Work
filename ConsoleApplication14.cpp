2029b350cc28daa1b67be8a6b0ba96f583480673
hryak3 <132013217+RasputkoTimur@users.noreply.github.com>
2024-04-06 16:52:25 +0300

    Add files via upload

---
+++ ConsoleApplication14.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int perevod(int n)
+{
+    int num = 0;
+    int a = 1;
+    int temp = n;
+
+    while (temp)
+    {
+        int last_num = temp % 10;
+        temp = temp / 10;
+        num += last_num * a;
+        a = a * 2;
+    }
+
+    return num;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int num;
+    cout << "Введите число: ";
+    cin >> num;
+
+    cout << "В переводе: " << perevod(num) <<endl;
+}
