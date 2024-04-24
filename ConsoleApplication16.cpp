283b8df6de6a5cf89f25fd139bfa4459b1fa3e5f
hryak3 <132013217+RasputkoTimur@users.noreply.github.com>
2024-04-24 19:30:47 +0300

    Add files via upload

--- ConsoleApplication16.cpp
+++ ConsoleApplication16.cpp
@@ -3,12 +3,21 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "rus");
+    srand(time(NULL));
     int size;
     cout << "Введите размер массива: ";
     cin >> size;
 
-    // Создаем динамический массив
     int* arr = new int[size];
 
     cout << "Адрес массива: " << arr << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 10;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
