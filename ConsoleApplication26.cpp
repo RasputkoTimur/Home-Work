782d20b73c6b1d567233c49385b8fba9b3802192
hryak3 <132013217+RasputkoTimur@users.noreply.github.com>
2024-05-15 11:21:02 +0300

    Add files via upload

---
+++ ConsoleApplication26.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+void quickSort(int* array, int left, int right)
+{
+    //int curIndex = left - 1;
+    if (left > right) return;
+    int marker = left - 1;
+    for (int curIndex = left; curIndex <= right; curIndex++)
+    {
+        if (array[curIndex] <= array[right])
+        {
+            marker++;
+            if (curIndex > marker)
+            {
+                swap(array[curIndex], array[marker]);
+            }
+        }
+    }
+    quickSort(array, left, marker - 1);
+    quickSort(array, marker - 1, right);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+}
+
