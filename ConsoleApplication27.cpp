a7ebd309aa4eddc641fa5f36f6323b5a237502d2
hryak3 <132013217+RasputkoTimur@users.noreply.github.com>
2024-05-21 12:55:02 +0300

    Add files via upload

---
+++ ConsoleApplication27.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    ifstream in("text.txt");
+    ofstream out("text(copy).txt");
+
+    int max_line_length;
+    cout << "Введите максимальную длину строки: ";
+    cin >> max_line_length;
+
+    string line;
+    while (getline(in, line)) {
+        if (line.length() <= max_line_length) {
+            out << line << endl;
+        }
+    }
+    in.close();
+    out.close();
+
+}
